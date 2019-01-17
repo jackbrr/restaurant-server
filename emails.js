b7e4aabb5080276d640c3090303291376e926ec4
jackbrr <jackbarr94@hotmail.co.uk>
2019-01-17 23:52:10 +0000

    emailsfinished

--- emails.js
+++ emails.js
@@ -1,12 +1,12 @@
 const sgMail = require('@sendgrid/mail');
 const Booking = require('./models/Booking');
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+sgMail.setApiKey('API KEY');
 
 function send(bookingInfo) {
     const msg = {
         to: bookingInfo.email,
-        from: 'jackbarr123@googlemail.com',
+        from: 'confirmation@diningroom.com',
         subject: 'Confirmation of your reservation',
         text: 'Hello, this email is to confirm the booking made for ' + bookingInfo.restaurant.name +  ' on the '  + bookingInfo.date + ' at ' + bookingInfo.time + ' for ' + bookingInfo.numPeople + ' people. Thank you for using Dining Room.',
         html: 'Hello,  this email is to confirm the booking made for ' + bookingInfo.restaurant.name +  ' on the '  + bookingInfo.date + ' at ' + bookingInfo.time + ' for ' + bookingInfo.numPeople + ' people. Thank you for using Dining Room.',
@@ -18,10 +18,10 @@ function send(bookingInfo) {
     function sendCancel(bookingInfo) {
      const msgCancel = {
         to: bookingInfo.email,
-        from: 'jackbarr123@googlemail.com',
+        from: 'cancellation@diningroom.com',
         subject: 'Cancellation of your reservation',
-        text: 'Hello, ',
-        html: 'Hello, ',
+        text: 'Hello, this email is to confirm the cancellation of your booking at  ' + bookingInfo.restaurant.name + ' on the ' + bookingInfo.date + ' at ' + bookingInfo.time + '. Thank you for using Dining Room. ',
+        html: 'Hello, this email is to confirm the cancellation of your booking at  ' + bookingInfo.restaurant.name + ' on the ' + bookingInfo.date + ' at ' + bookingInfo.time + '. Thank you for using Dining Room. ',
    };
     return sgMail.send(msgCancel);
 }
